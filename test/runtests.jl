f9e6d2fa25e35fe84cd3f35997671fd5832b9efb
william davis <williamjsdavis@berkeley.edu>
2020-11-30 20:35:18 -0800

    Mode grid tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,13 +5,13 @@ Grid(5., 5., 64, 64)
 
 @testset "NavierStokes2D.jl" begin
 
-    test_grid = Grid(1., 2., 32, 64)
+    test_grid = Grid(1., 2., 16, 32)
     @test test_grid.xmax == 1.0
     @test test_grid.ymax == 2.0
-    @test test_grid.nx == 32
-    @test test_grid.ny == 64
-    @test size(test_grid.xRange) == (32,)
-    @test size(test_grid.yRange) == (64,)
+    @test test_grid.nx == 16
+    @test test_grid.ny == 32
+    @test size(test_grid.xRange) == (16,)
+    @test size(test_grid.yRange) == (32,)
     @test isa(test_grid.Δx, Float64)
     @test isa(test_grid.Δy, Float64)
 
